d41ec0a4b3c970ef5b927bb1d44ba8967d1caf42
pranav8553 <pranavCIC065@gmail.com>
2020-08-14 17:28:15 +0530

    Added: Calculated PartTime and FullTime wage of employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,22 @@
 public class EmployeeWage{
 	//Constants
-	public static final int IS_FULL_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static void main(String[] args){
 		//Variables
 		int empHours;
 		int employeeWage;
-		int randomCheck=(int)Math.floor(Math.random()*10)%2;
+		int randomCheck=(int)Math.floor(Math.random()*10)%3;
 		//Computation
 		if (randomCheck == IS_FULL_TIME){
 			empHours=8;
 		}
-		else{
-			empHours=0;
+		else if (randomCheck == IS_PART_TIME){
+			empHours=4;
 		}
+		else
+			empHours=0;
 		employeeWage=empHours * EMP_RATE_PER_HOUR;
 		System.out.println("Employee wage: "+employeeWage);
 	}
